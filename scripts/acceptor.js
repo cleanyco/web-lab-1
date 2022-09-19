a8053f1493c1dc0df0b3b10f3c83d4f00e65a9f9
grxuau <henryycampbell@gmail.com>
2022-09-19 22:15:46 +0300

    gonna edit form

--- scripts/acceptor.js
+++ scripts/acceptor.js
@@ -1,18 +1,10 @@
-function submitValidation() {
-    const formElementXY = document.getElementById('xyform');
-    formElementXY.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const formDataXY = new FormData(formElemXY); 
-        const x = formDataXY.get('xcoord');
-        const y = formDataXY.get('ycoord');
-      });
+const x = document.getElementById('xcoord')
+const y = document.getElementById('ycoord')
+const form = document.getElementById('xyform')
 
-    const formElementR = document.getElementById('rform')
-    formElementR.addEventListener('radio', (e) => {
-        e.preventDefault();
-        const formDataR = new FormData(formElementR);
-        const r = formDataR.get('radius');
-    });
+form.addEventListener('submit', (e => {
+    e.preventDefault()
+}))
 
     if ((y > 5) || (y < -5) || (!y.isFinite())) {
         console.log('Что-то тут не так...');

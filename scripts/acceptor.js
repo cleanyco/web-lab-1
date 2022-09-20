e5630954ac0cb6d8723f62eb2720821538d05789
grxuau <henryycampbell@gmail.com>
2022-09-20 08:52:05 +0300

    writing regex

--- scripts/acceptor.js
+++ scripts/acceptor.js
@@ -1,21 +1,53 @@
-const x = document.getElementById('xcoord')
-const y = document.getElementById('ycoord')
-const form = document.getElementById('xyform')
+var x = document.getElementById('xcoord')
+//FIXME
+var y = document.getElementById('ycoord')
+var r = document.getElementById('radius')
+const form = document.getElementById('form')
+var errorElement = document.getElementById('error')
 
-form.addEventListener('submit', (e => {
-    e.preventDefault()
-}))
+const regexDifferentBase = '(0|0x|e|Ob|0B)[0-9].*';
+//FIXME
+const regexCommaInTheNumber = '\d+,\d*'
 
-    if ((y > 5) || (y < -5) || (!y.isFinite())) {
-        console.log('Что-то тут не так...');
-        return false;
-    } else {
-        console.log('Всё чисто');
-        return true;
-        
+//FIXME print only one error message (?)
+function validateForm(y) {
+    form.addEventListener('submit', (e => {
+        e.preventDefault()
+        let message = ''
+
+        // if (yNumeric == null) {
+        //     messages.push('Number cannot be null!')
+        //     errorElement.innerText = messages.join(', ')
+        // }
+
+        if (y.value.match(regexDifferentBase)) {
+            message = 'Required input in decimal!'
+            errorElement.innerText = message
+            return false
+        }
+
+        if (y.value.match(regexCommaInTheNumber)) {
+            message = 'Use a dot instead of a comma!'
+            errorElement.innerText = message
+
+        }
+
+        const yNumeric = Number(y.value.trim());
+        debugger
+
+        if (yNumeric === NaN) {
+            message = 'Invalid input!'
+            errorElement.innerText = message
+            return false
+        }
+
+        //FIXME check should be at the end
+        if (yNumeric > 5 || yNumeric < -5) {
+            message = 'Number out of range!'
+            errorElement.innerText = message
+            return false
+        }
         // you cannot use a comma instead of a period, the number cannot start from zero, 
-        // there should not be extra characters, you need to allow spaces at the beginning and end, but prohibit them in the middle
-
-
-    }
+    //         // there should not be extra characters, you need to allow spaces at the beginning and end, but prohibit them in the middle
+    })) 
 }

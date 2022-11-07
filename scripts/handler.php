6cca2292e5f2569948abf3344249d39a63dd3efa
grxuau <henryycampbell@gmail.com>
2022-11-07 11:32:37 +0300

    general fixes

---
+++ scripts/handler.php
@@ -0,0 +1,79 @@
+<?php
+
+function validateX($xVal): bool
+{
+    return isset($xVal);
+}
+
+function validateY($yVal): bool
+{
+    $Y_MIN = -5;
+    $Y_MAX = 5;
+
+    if (!isset($yVal))
+        return false;
+
+    $numY = str_replace(',', '.', $yVal);
+    return is_numeric($numY) && $numY >= $Y_MIN && $numY <= $Y_MAX;
+}
+
+function validateR($rVal): bool
+{
+    return isset($rVal);
+}
+
+function validateForm($xVal, $yVal, $rVal): bool
+{
+    return validateX($xVal) && validateY($yVal) && validateR($rVal);
+}
+
+
+function checkTriangle($xVal, $yVal, $rVal): bool
+{
+    return $xVal >= 0 && $yVal >= 0 &&
+        $yVal <= $xVal + $rVal/2;
+}
+
+function checkRectangle($xVal, $yVal, $rVal): bool
+{
+    return $xVal <= 0 && $xVal >= $rVal && $yVal >= $rVal && $yVal <= $rVal;
+}
+
+function checkCircle($xVal, $yVal, $rVal): bool
+{
+    return $xVal <=0 && $yVal <= 0 &&
+        sqrt($xVal*$xVal + $yVal*$yVal) <= $rVal/2;
+}
+
+function checkHit($xVal, $yVal, $rVal): bool
+{
+    return checkTriangle($xVal, $yVal, $rVal) || checkRectangle($xVal, $yVal, $rVal) ||
+        checkCircle($xVal, $yVal, $rVal);
+}
+
+
+$xVal = $_POST['x'];
+$yVal = $_POST['y'];
+$rVal = $_POST['r'];
+
+$timezoneOffset = $_POST['date'];
+
+$isValid = validateForm($xVal, $yVal, $rVal);
+$converted_isValid = $isValid ? 'true' : 'false';
+$isHit = $isValid ? checkHit($xVal, $yVal, $rVal) : 'put dispenser';
+$converted_isHit = $isHit ? 'true' : 'false';
+
+$currentTime = date('H:i:s', time()-$timezoneOffset*60);
+$executionTime = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 7);
+
+$jsonData = '{' .
+    "\"validate\":$converted_isValid," .
+    "\"xval\":\"$xVal\"," .
+    "\"yval\":\"$yVal\"," .
+    "\"rval\":\"$rVal\"," .
+    "\"curtime\":\"$currentTime\"," .
+    "\"exectime\":\"$executionTime\"," .
+    "\"hitres\":$converted_isHit" .
+    "}";
+
+echo $jsonData;
---
+++ scripts/validator.js
@@ -0,0 +1,127 @@
+$(function() {
+    let yCoord = undefined
+    let xCoord = undefined
+    let rCoord = undefined
+    const form = document.getElementById('form')
+
+//fixme можно вводить начиная с нуля
+//fixme сделать все требования по наследованию элементов
+//fixme проверить инъекцию
+    function check_y() {
+        let valid;
+        const startsWithZero = new RegExp("0+\\d+");
+        const numberSystems = new RegExp("(0x|0o|0b)\d*")
+        const yElement = document.getElementById('ycoord').value
+        const y = yElement.replace(',', '.').trim();
+        if (isNaN(y) || isNaN(parseFloat(y))) {
+            alert('Invalid input!')
+            valid = false;
+        } else if (parseFloat(y) < -5 || parseFloat(y) > 5) {
+            alert('Please enter a number between -5 and 5')
+            valid = false;
+        } else if (startsWithZero.test(y)){
+            alert('Number cannot starts with zero!')
+            valid = false;
+        } else if (numberSystems.test(y)) {
+            alert("You can only enter numbers in decimal notation!");
+            valid = false;
+        } else {
+            yCoord = y
+            valid = true;
+        }
+        return valid;
+    }
+
+    function check_x() {
+        let valid;
+        const startsWithZero = new RegExp("0+\\d+");
+        const numberSystems = new RegExp("(0x|0o|0b)\d*")
+        //fixme
+        const xElement = document.querySelector('input[name="xcoord"]:checked').value
+        const x = xElement.replace(',', '.').trim();
+        if (isNaN(x) || isNaN(parseFloat(x))) {
+            alert('Invalid input!')
+            valid = false;
+        } else if (parseFloat(x) < -5 || parseFloat(x) > 3) {
+            alert('Please enter a number between -5 and 5')
+            valid = false;
+        } else if (startsWithZero.test(x)){
+            alert('Number cannot starts with zero!')
+            valid = false;
+        } else if (numberSystems.test(x)) {
+            alert("You can only enter numbers in decimal notation!");
+            valid = false;
+        } else {
+            xCoord = x
+            valid = true;
+        }
+        return valid;
+    }
+
+    function check_r() {
+        let valid;
+        const startsWithZero = new RegExp("0+\\d+");
+        const numberSystems = new RegExp("(0x|0o|0b)\d*")
+        const rElement = document.querySelector('input[name="rcoord"]:checked').value
+        const r = rElement.replace(',', '.').trim();
+        if (isNaN(r) || isNaN(parseFloat(r))) {
+            alert('Invalid input!')
+            valid = false;
+        } else if (parseFloat(r) < -5 || parseFloat(r) > 3) {
+            alert('Please enter a number between -5 and 5')
+            valid = false;
+        } else if (startsWithZero.test(r)){
+            alert('Number cannot starts with zero!')
+            valid = false;
+        } else if (numberSystems.test(r)) {
+            alert("You can only enter numbers in decimal notation!");
+            valid = false;
+        } else {
+            rCoord = r
+            valid = true;
+        }
+        return valid;
+    }
+
+    function sendPost() {
+        const xVal = document.querySelector('input[name="xcoord"]:checked').value
+        const rVal = document.querySelector('input[name="rcoord"]:checked').value
+        $.ajax({
+            url: './scripts/handler.php',
+            method: 'POST',
+            data: {
+                x: xVal,
+                y: yCoord,
+                r: rVal,
+                date: new Date().getTimezoneOffset()
+            },
+            dataType: 'json',
+            // beforeSend: function() {
+            //     $('.button').attr('disabled', 'disabled');
+            // },
+            success: function(data) {
+                // $('.button').attr('disabled', false);
+                if (check_y() && check_y() && check_r()) {
+                    newRow = '<tr>';
+                    newRow += '<td>' + data.xval + '</td>';
+                    newRow += '<td>' + data.yval + '</td>';
+                    newRow += '<td>' + data.rval + '</td>';
+                    newRow += '<td>' + data.curtime + '</td>';
+                    newRow += '<td>' + data.exectime + '</td>';
+                    newRow += '<td>' + data.hitres + '</td>';
+                    $('#result-table').append(newRow);
+                }
+            },
+            error: function(jqXHR, textStatus) {
+                alert(`Запрос не был отправлен! Причина: ${jqXHR.statusText}`)
+            }
+        });
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if (check_x() && check_y() && check_r()) {
+            sendPost()
+        }
+    })
+});
